fec0ee5b3201bdad6b05e9839ccd1417f60bdfdc
zeepk <mhughes.ipod@gmail.com>
2022-01-20 23:52:23 -0800

    procfile

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
-import dotenv from 'dotenv';
-import Discord from 'discord.js';
-import getResponse from './runCommand.js';
+import dotenv from "dotenv";
+import Discord from "discord.js";
+import express from "express";
+import getResponse from "./runCommand.js";
 dotenv.config();
 
 const client = new Discord.Client({
-	allowedMentions: {
-		parse: ['users', 'roles'],
-		repliedUser: true,
-	},
-	intents: [
-		'GUILDS',
-		'GUILD_MESSAGES',
-		'GUILD_PRESENCES',
-		'GUILD_MEMBERS',
-		'GUILD_MESSAGE_REACTIONS',
-	],
+  allowedMentions: {
+    parse: ["users", "roles"],
+    repliedUser: true,
+  },
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES",
+    "GUILD_PRESENCES",
+    "GUILD_MEMBERS",
+    "GUILD_MESSAGE_REACTIONS",
+  ],
 });
 
-client.on('ready', () => {
-	console.log('WildlifeBot Running...');
+client.on("ready", () => {
+  console.log("WildlifeBot Running...");
 });
 
 client.login(process.env.BOT_TOKEN);
 
-client.on('messageCreate', async (message) => {
-	const content = message.content;
-	if (!content) {
-		return;
-	}
-	const response = await getResponse(content);
-	if (response) {
-		message.channel.send(response);
-	}
-	// if (content === 'test') {
-	// }
-	// if (content === '!commands') {
-	// 	message.channel.send(commands.commands);
-	// }
+client.on("messageCreate", async (message) => {
+  const content = message.content;
+  if (!content) {
+    return;
+  }
+  const response = await getResponse(content);
+  if (response) {
+    message.channel.send(response);
+  }
+  // if (content === 'test') {
+  // }
+  // if (content === '!commands') {
+  // 	message.channel.send(commands.commands);
+  // }
 
-	// if (content.indexOf('!villager ') === 0) {
-	// 	const searchTerm = content.split('!villager ')[1];
-	// 	message.channel.send(constants.nookipediaUrlPrefix + searchTerm);
-	// }
+  // if (content.indexOf('!villager ') === 0) {
+  // 	const searchTerm = content.split('!villager ')[1];
+  // 	message.channel.send(constants.nookipediaUrlPrefix + searchTerm);
+  // }
 });
+
+var app = express();
+app.post("/send", function (req, res) {
+  console.log(req);
+  client.channels.cache.get("933990639178252308").send("howdy");
+  res.send("hello world");
+});
+app.listen(8000, () => console.log("app listening on port 8000"));
--- runCommand.js
+++ runCommand.js
@@ -1,105 +1,105 @@
-import commandList from './commands.js';
+import commandList from "./commands.js";
 import {
-	startsWith,
-	formatCritterResponse,
-	formatVillagerResponse,
-} from './helperFunctions.js';
-import { CritterType } from './constants.js';
-import fetch from 'node-fetch';
+  startsWith,
+  formatCritterResponse,
+  formatVillagerResponse,
+} from "./helperFunctions.js";
+import { CritterType } from "./constants.js";
+import fetch from "node-fetch";
 
 const getResponse = async (message) => {
-	const commands = commandList;
+  const commands = commandList;
 
-	// check to see if the command is in the list of static commands
-	const staticCommands = commands.staticCommands;
-	const staticCommand = staticCommands.find((cmd) => cmd.command === message);
-	if (staticCommand) {
-		return staticCommand.response;
-	}
+  // check to see if the command is in the list of static commands
+  const staticCommands = commands.staticCommands;
+  const staticCommand = staticCommands.find((cmd) => cmd.command === message);
+  if (staticCommand) {
+    return staticCommand.response;
+  }
 
-	// if not, must be a dynamic command
+  // if not, must be a dynamic command
 
-	// villager/ac/wiki
-	if (
-		startsWith(message, commands.ac.command) ||
-		startsWith(message, commands.wiki.command)
-	) {
-		const searchTerm = message.split(' ')[1].replace(/ /g, '_');
-		return `${commands.wiki.response}${searchTerm}`;
-	}
+  // villager/ac/wiki
+  if (
+    startsWith(message, commands.ac.command) ||
+    startsWith(message, commands.wiki.command)
+  ) {
+    const searchTerm = message.split(" ")[1].replace(/ /g, "_");
+    return `${commands.wiki.response}${searchTerm}`;
+  }
 
-	// fish
-	if (startsWith(message, commands.fish.command)) {
-		const response = await fetch(commands.fish.response);
-		const apiResponse = await response.json();
-		const searchTerm = message
-			.split(commands.fish.command)[1]
-			.toLowerCase()
-			.replace(/ /g, '_');
+  // fish
+  if (startsWith(message, commands.fish.command)) {
+    const response = await fetch(commands.fish.response);
+    const apiResponse = await response.json();
+    const searchTerm = message
+      .split(commands.fish.command)[1]
+      .toLowerCase()
+      .replace(/ /g, "_");
 
-		const data = apiResponse[searchTerm];
-		if (!data) {
-			return `Unable to find ${searchTerm} in the fish database.`;
-		}
+    const data = apiResponse[searchTerm];
+    if (!data) {
+      return `Unable to find ${searchTerm} in the fish database.`;
+    }
 
-		return formatCritterResponse(data, CritterType.FISH);
-	}
+    return formatCritterResponse(data, CritterType.FISH);
+  }
 
-	// bugs
-	if (startsWith(message, commands.bugs.command)) {
-		const response = await fetch(commands.bugs.response);
-		const apiResponse = await response.json();
-		const searchTerm = message
-			.split(commands.bugs.command)[1]
-			.toLowerCase()
-			.replace(/ /g, '_');
+  // bugs
+  if (startsWith(message, commands.bugs.command)) {
+    const response = await fetch(commands.bugs.response);
+    const apiResponse = await response.json();
+    const searchTerm = message
+      .split(commands.bugs.command)[1]
+      .toLowerCase()
+      .replace(/ /g, "_");
 
-		const data = apiResponse[searchTerm];
-		if (!data) {
-			return `Unable to find ${searchTerm} in the bugs database.`;
-		}
+    const data = apiResponse[searchTerm];
+    if (!data) {
+      return `Unable to find ${searchTerm} in the bugs database.`;
+    }
 
-		return formatCritterResponse(data, CritterType.BUGS);
-	}
+    return formatCritterResponse(data, CritterType.BUGS);
+  }
 
-	// sea creatures
-	if (startsWith(message, commands.sea.command)) {
-		const response = await fetch(commands.sea.response);
-		const apiResponse = await response.json();
-		const searchTerm = message
-			.split(commands.sea.command)[1]
-			.toLowerCase()
-			.replace(/ /g, '_');
+  // sea creatures
+  if (startsWith(message, commands.sea.command)) {
+    const response = await fetch(commands.sea.response);
+    const apiResponse = await response.json();
+    const searchTerm = message
+      .split(commands.sea.command)[1]
+      .toLowerCase()
+      .replace(/ /g, "_");
 
-		const data = apiResponse[searchTerm];
-		if (!data) {
-			return `Unable to find ${searchTerm} in the sea creatures database.`;
-		}
+    const data = apiResponse[searchTerm];
+    if (!data) {
+      return `Unable to find ${searchTerm} in the sea creatures database.`;
+    }
 
-		return formatCritterResponse(data, CritterType.SEA);
-	}
+    return formatCritterResponse(data, CritterType.SEA);
+  }
 
-	// villagers
-	if (startsWith(message, commands.villager.command)) {
-		const response = await fetch(commands.villager.response);
-		const apiResponse = await response.json();
-		const searchTerm = message
-			.split(commands.villager.command)[1]
-			.replace(/(?:^|\s|["'([{])+\S/g, (match) => match.toUpperCase());
+  // villagers
+  if (startsWith(message, commands.villager.command)) {
+    const response = await fetch(commands.villager.response);
+    const apiResponse = await response.json();
+    const searchTerm = message
+      .split(commands.villager.command)[1]
+      .replace(/(?:^|\s|["'([{])+\S/g, (match) => match.toUpperCase());
 
-		const villagerList = Object.entries(apiResponse).map(
-			(keyValuePair) => keyValuePair[1],
-		);
-		const data = villagerList.find(
-			(v) => v['name']['name-USen'] === searchTerm,
-		);
-		if (!data) {
-			return `Unable to find ${searchTerm} in the villagers database.`;
-		}
+    const villagerList = Object.entries(apiResponse).map(
+      (keyValuePair) => keyValuePair[1]
+    );
+    const data = villagerList.find(
+      (v) => v["name"]["name-USen"] === searchTerm
+    );
+    if (!data) {
+      return `Unable to find ${searchTerm} in the villagers database.`;
+    }
 
-		return formatVillagerResponse(data);
-	}
+    return formatVillagerResponse(data);
+  }
 
-	return '';
+  return "";
 };
 export default getResponse;
